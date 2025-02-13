cc9943b2c8e369b6f435a4b6af3348f9350681f2
Dragons0up <jungyongguk5@gmail.com>
2025-02-13 18:50:11 +0900

    조금 더 수정했던거

--- gamescreen/timer/timer.js
+++ gamescreen/timer/timer.js
@@ -1,6 +1,6 @@
 var container = document.querySelector(".timer")
-var min = 1;
-var sec = (min*60);
+var min = 0;
+var sec = (min*60) + 11;
 var num = 360;
 
 /**
@@ -10,11 +10,11 @@ function startTimer() {
     const timerId = setInterval(() => {
         sec--;
 
-        container.style.setProperty("--timerA",num+"deg")
+        container.style.setProperty("--timerA", num + "deg")
         container.style.background = ` conic-gradient(#E1E2E7 var(--timerA) ,#E1E2E7 0deg ,#293047 0deg,#293047 360deg)`
         num = num - (num / sec);
 
-        document.getElementById('timerDisplay').textContent = sec + "초";
+        document.getElementById('timerDisplay').textContent = sec - 1 + "초";
 
         if (sec <= 0) {
             clearInterval(timerId);
